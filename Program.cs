f9b922603932f53170c6fc415c5cdf6fbeddf27c
Michał Wilewski <55109192+Dmpoaod@users.noreply.github.com>
2022-10-24 14:45:03 +0200

    2nd commit

--- Program.cs
+++ Program.cs
@@ -5,59 +5,31 @@ using System.Text.RegularExpressions;
 
 namespace Zad1
 {
+    public enum EHeroClass
+        {
+            barbarzyńca, 
+            paladyn, 
+            amazonka,
+        }
+    public class Hero
+    {
+        public string name;
+        public EHeroClass Class;
+
+        public Hero(string Hname, EHeroClass HClass)
+        {
+            name = Hname;
+            Class = HClass;
+        }
+        
+    }
     class Program
     {
         
         public static string patternText = @"^[a-zA-Z ]{2,}";
         public static Regex reg = new Regex(patternText);
         
-        // static int removeWS(char[] str)
-        // {
-        //     int cnt = 0;
-        //     for (int i = 0; i < str.Length; i++)
-        //     {
-        //         if (str[i] != ' ')
-        //         {
-        //             str[cnt++] = str[i]; 
-        //         }
-        //     }
-        //     return cnt;
-        // }
-        //
-        // static bool validateName(char[] str)
-        // {
-        //     bool res = true;
-        //     int spcCount = 0;
-        //     if (str.Length < 2)
-        //     {
-        //         res = false;
-        //         return res;
-        //     }
-        //     else
-        //     {
-        //         for (int i = 0; i < str.Length; i++)
-        //         {
-        //             if (str[i].Equals(' '))
-        //             {
-        //                 spcCount++;
-        //             }
-        //             if (!Char.IsLetter(str[i]) || !str[i].Equals(' '))
-        //             {
-        //                 res = false;
-        //                 return res;
-        //             }
-        //         }
-        //
-        //         if (str.Length - spcCount < 2)
-        //         {
-        //             res = false;
-        //             return res;
-        //         }
-        //         
-        //     }
-        //     return res;
-        // }
-
+       
         static bool validate(string str)
         {
             bool res = false;
@@ -75,9 +47,9 @@ namespace Zad1
         {
             string firstOption;
             string heroName;
-            Console.WriteLine("Witaj w grze Pierwsza Przygoda\n");
-            Console.WriteLine("[1] Zacznij nową grę\n");
-            Console.WriteLine("[X] Zamknij program\n");
+            Console.WriteLine("Witaj w grze Pierwsza Przygoda");
+            Console.WriteLine("[1] Zacznij nową grę");
+            Console.WriteLine("[X] Zamknij program");
             firstOption = Console.ReadLine();
             if (String.Equals(firstOption, "X") || String.Equals(firstOption, "x"))
             {
@@ -87,12 +59,34 @@ namespace Zad1
                 Console.Clear();
                 Console.WriteLine("Podaj imię bohatera");
                 heroName = Console.ReadLine();
-                heroName.Trim();
+                heroName = heroName.Trim();
                 while (validate(heroName) == false)
                 {
                     Console.WriteLine("Imię niepoprwne, wpisz ponownie");
                     heroName = Console.ReadLine();
                 }
+                
+                Console.WriteLine("Witaj "+ heroName +". Wybierz klasę postaci. Dostępne klasy: ");
+                Console.WriteLine("[1] Barbarzyńca");
+                Console.WriteLine("[2] Paladyn");
+                Console.WriteLine("[3] Amazonka");
+                string input = Console.ReadLine();
+                EHeroClass HClass = EHeroClass.barbarzyńca;
+                if (input.Equals("1"))
+                {
+                    HClass = EHeroClass.barbarzyńca;
+                } else if (input.Equals("2"))
+                {
+                    HClass = EHeroClass.paladyn;
+                } else if (input.Equals("3"))
+                {
+                    HClass = EHeroClass.amazonka;
+                }
+                
+                Hero hero = new Hero(heroName, HClass);
+                Console.Clear();
+                Console.WriteLine(hero.Class + " " + hero.name + " wyrusza na przygodę");
+
 
             }
             
