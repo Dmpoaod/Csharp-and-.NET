70e2d6882b03269c7cec8783bc6ea4560e352804
Michał Wilewski <55109192+Dmpoaod@users.noreply.github.com>
2022-10-25 13:23:47 +0200

    5th commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Text.RegularExpressions;
 
 namespace Zad1
 {
+    
     public enum EHeroClass
         {
             barbarzyńca, 
@@ -26,7 +27,7 @@ namespace Zad1
         }
         
     }
-
+    
     public class NonPlayerCharacter
     {
         public string name;
@@ -34,34 +35,65 @@ namespace Zad1
         public NonPlayerCharacter(string NPCname)
         {
             name = NPCname;
-
-            void NpcDialogPartStartTalking()
-            {
-                
-            }
-
+            
         }
+        void NpcDialogPartStartTalking()
+                     {
+                         
+                     }
     }
 
     public class Location
     {
         public string locationName;
         public NonPlayerCharacter[] NPCList = new NonPlayerCharacter[10];
+
+        public Location(string name, NonPlayerCharacter[] NpcList)
+        {
+            locationName = name;
+            NPCList = NpcList;
+        }
+    }
+
+    public class DialogParser
+    {
+        
     }
 
     public class NpcDialogPart
     {
-        private string[] HDiag = new string[] { "Tak, chętnie pomogę", "„Dam znać jak będę gotowy", "100 sztuk złota to za mało!", "OK, może być 100 sztuk złota", "W takim razie radź sobie sam.", "Nie, nie pomogę, żegnaj" };
+         string[] NpcDiag = new string[]
+        {
+            "Witaj, czy możesz mi pomóc dostać się do innego miasta?", "Dziękuję! W nagrodę otrzymasz ode mnie 100 sztuk złota", "Niestety nie mam więcej. Jestem bardzo biedny", "Dziękuję",
+            "Witaj, czy chcesz coś kupić?", "Oto one", "70 sztuk złota", "Nie zejdę niżej niż 50", "Do zobaczenia"
+        };
+        
+         string[] HDiag = new string[]
+         {
+             "Tak, chętnie pomogę", "„Dam znać jak będę gotowy", "100 sztuk złota to za mało!", "OK, może być 100 sztuk złota", "W takim razie radź sobie sam.", "Nie, nie pomogę, żegnaj",
+             "Chciałbym zobaczyć twoje towary", "Ile za to?", "To za drogo. Mogę dać 30.", "Uczciwa cena. Kupuję.", "Nie zapłacę tyle", "Żegnaj"
+         };
+         
 
     }
 
     public class HeroDialogPart
     {
-        
+        string[] HDiag = new string[]
+        {
+            "Tak, chętnie pomogę", "„Dam znać jak będę gotowy", "100 sztuk złota to za mało!", "OK, może być 100 sztuk złota", "W takim razie radź sobie sam.", "Nie, nie pomogę, żegnaj",
+            "Chciałbym zobaczyć twoje towary", "Ile za to?", "To za drogo. Mogę dać 30.", "Uczciwa cena. Kupuję.", "Nie zapłacę tyle", "Żegnaj"
+        };
     }
     
     class Program
     {
+        void ShowLocation(Location location)
+        {
+        
+        }
+
+    
         
         public static string patternText = @"^[a-zA-Z ]{2,}";
         public static Regex reg = new Regex(patternText);
@@ -123,6 +155,29 @@ namespace Zad1
                 Hero hero = new Hero(heroName, HClass);
                 Console.Clear();
                 Console.WriteLine(hero.Class + " " + hero.name + " wyrusza na przygodę");
+
+                NonPlayerCharacter[] npclist = new NonPlayerCharacter[]
+                    { new NonPlayerCharacter("Akara"), new NonPlayerCharacter("Cain") };
+
+                Location loc = new Location("Neverwinter", npclist);
+                
+                Console.WriteLine("Znajdujesz się w: " + loc.locationName + ". Co chcesz zrobić?");
+                Console.WriteLine("[1] Porozmawiaj z " + npclist[0].name + ".");
+                Console.WriteLine("[2] Porozmawiaj z " + npclist[1].name + ".");
+                Console.WriteLine("[X] Zamknij program.");
+                firstOption = Console.ReadLine();
+                if (String.Equals(firstOption, "X") || String.Equals(firstOption, "x"))
+                {
+                    System.Environment.Exit(0); 
+                } else if (String.Equals(firstOption, "1"))
+                {
+                    
+                } else if (String.Equals(firstOption, "2"))
+                {
+                    
+                }
+
+
 
 
             }
